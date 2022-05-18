d0cbb8527b31d8ab8b24d008d1994da5e2081fa4
Karim El Jed <karim.eljed@exxeta.com>
2022-05-18 17:31:16 +0200

    Improve interface documentation

--- SemanticMocking.Abstractions/IMockBehaviour.cs
+++ SemanticMocking.Abstractions/IMockBehaviour.cs
@@ -1,7 +1,7 @@
 namespace SemanticMocking.Abstractions;
 
 /// <summary>
-/// Just a marker interface.
+/// Just a marker interface to allow casting to concrete mocking framework implementations. 
 /// </summary>
 public interface IMockBehaviour
 {
